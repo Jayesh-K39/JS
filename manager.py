2129a46557452475220677c7b1b54c5e2f61aaec
Your Real Name <your@email.com>
2025-06-07 15:52:24 +0530

    Updated JS on Sat Jun  7 03:52:24 PM IST 2025

--- manager.py
+++ manager.py
@@ -2,19 +2,19 @@ import os
 import shutil
 
 
-src = "/home/jk/js/chapter_1_practice_set"
-dest = "/home/jk/js"
+src = "/home/jk/js/decorator.py"
+dest = "/home/jk/py"
 
 #os.rename(src, dest)
-
+shutil.move(src,dest)
 #shutil.copy2(src, dest)
 #os.symlink("/home/jk/C/C_practice", "cp")
 
-items =  [f for f in os.listdir(src) if os.path.isfile(os.path.join(src, f))]
+'''items =  [f for f in os.listdir(src) if os.path.isfile(os.path.join(src, f))]
 for a in items:
     src_path = os.path.join(src, a)
     dest_path = os.path.join(dest, a)
-    shutil.move(src_path, dest_path)
+    shutil.move(src_path, dest_path)'''
 
 
 print(" Mission Successful !")
---
+++ while_loops.js
@@ -0,0 +1,23 @@
+let a = 5
+a = Number.parseInt(a)
+let i = 1
+
+while (i <= a){
+	console.log(i)
+	i++
+}
+
+
+console.log('\nNow, the do while loops: \n')
+let k = 5; let j = 1;
+do{
+	console.log(j)
+	j++
+}
+while (j <= k)
+
+
+// While Loops check the condition first. If the condition is satisfied, the loop body is executed.
+
+//Do while execute the loop body first, no matter th conditions are met or not. Then, from the second iteration, they check teh conditions. If those are met, they execute the loop body again, else they stop (or do whatever is supposed to be done)
+
