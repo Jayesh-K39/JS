a7a1480a5f311e25f043a71fd496ba1198d971ed
Your Real Name <your@email.com>
2025-06-09 21:21:02 +0530

    Updated Projects on Mon Jun  9 09:21:02 PM IST 2025

--- counting.py
+++ counting.py
@@ -1,3 +1,10 @@
 import os
-filenum = sum(1 for item in os.listdir() if os.path.isfile(item))
-print(f"There are {filenum} files in this directory")
+filenum = sum(1 for item in os.listdir() if os.path.isfile(item) and not item.startswith('.'))
+allnum = sum(1 for item in os.listdir() if os.path.isfile(item))
+print(f"There are {filenum} files in this directory (excluding the hidden ones)\n\nAnd {allnum} including the hidden ones.")
+
+
+
+#hidden = (file for file in os.listdir() if os.path.isfile(file) and file.startswith('.') )
+print(f"\nThe hidden ones are: {list(file for file in os.listdir() if os.path.isfile(file) and file.startswith('.'))}")
+
