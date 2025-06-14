4acaeb90ad60dee96cd0bf28ffe7953d31dbf519
Your Real Name <your@email.com>
2025-06-14 22:16:54 +0530

    Updated JS on Sat Jun 14 10:16:54 PM IST 2025

---
+++ alert_prompt_confirm.js
@@ -0,0 +1,12 @@
+alert('I am proud of ')
+let a = prompt('Enter a here: ', 'jayu')
+alert('You entered a of type' + (typeof a))
+let revenge = confirm('Yugandhar ka rape chhakko se karwana chahiye ya nahi?')
+
+if (revenge){
+	alert("That's the spirit!")
+}
+else{
+	alert("Either you are not Jayesh or you forgot the horrors of 2022")
+}
+//These can be used, but these block the script execution in order to interact with the user. Also, these give the website a very old ahh look. 
---
+++ console_object.js
@@ -0,0 +1,14 @@
+//Try this entire script in browser, to get the real taste.
+
+console.clear() //Literally clears the entire console
+console.assert(5>54)
+console.assert(53>5) //True, so won't return anything
+
+
+obj = {1:'Shwetabh', 2:'Guido', 3:'Denis'}
+console.table(obj)
+console.warn('Beware of minor bugs!')
+console.info('Hey dawg, dont skip this information.')
+console.error('Uh oh! Error in your code, bro!')
+console.time(start)
+console.time(end)
